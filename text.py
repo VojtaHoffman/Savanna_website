79d438187512a38462f3f00696a06f870e700104
Volt <117681129+VojtaHoffman@users.noreply.github.com>
2024-01-02 16:30:26 +0100

    Add files via upload

---
+++ text.py
@@ -0,0 +1,10 @@
+numbers = [5.2, 7.3, 12.1, 17, 21.4, 26, 28.5, 28.7, 24.5, 18.1, 11.9, 6.4]
+
+# Calculate the sum of the numbers
+total = sum(numbers)
+
+# Calculate the average
+average = total / len(numbers)
+
+# Print the average
+print("The average is:", average)
